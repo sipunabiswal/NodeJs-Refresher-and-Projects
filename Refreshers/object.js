1c2ec70b946e9b485114a3c5213ab0681015133a
sipuna.biswal <sipuna.biswal@gmail.com>
2022-10-02 10:35:22 +0530

    Spread and rest understanding

--- Refreshers/object.js
+++ Refreshers/object.js
@@ -17,8 +17,19 @@ for(let h of hobbies){
     console.log(h);
 }
 
-//Map : Map basically creates an edited array
+hobbies.push('Swiming');
 
+//Map : Map basically creates an edited array
 console.log(hobbies.map(h=>'Hobby: '+ h));
 console.log(hobbies);
 
+//Spread and rest
+const copiedHobbies=[...hobbies,'Drawing'];//Spread: will pull out the values from array and copies into an new array with new value.
+console.log(copiedHobbies);
+
+
+const toArray=(...args)=>{////Rest, Will merge elements into array from arguments.
+    return args;
+}
+
+console.log(toArray(1,2,3,4,5));
